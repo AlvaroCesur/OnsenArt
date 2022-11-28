f933388f7f8bfc0f6f78754f66f5660221efc04e
agonzalezrando <93986162+AlvaroCesur@users.noreply.github.com>
2022-11-28 18:59:55 +0100

    contacto+css vario

--- js/main.js
+++ js/main.js
@@ -112,3 +112,19 @@ function add_row()
  document.getElementById("new_stock").value="";
 
 }
+
+/* FUNCIONALIDAD MAPA */
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('background-map'), {
+    center: {lat: 36.719753548933475, lng: -4.365634167338904},
+    zoom: 10
+  });
+}
+//center-map
+ if (navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition(function (position) {
+         initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+         map.setCenter(initialLocation);
+     });
+ }
