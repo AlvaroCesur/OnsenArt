25f85831443e1ea4b120553f129e8fe99a0aaf35
Lizzy <alicia.s273596@cesurformacion.com>
2022-11-28 16:42:21 +0100

    Cambios chikitos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+    /* Menú */
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
@@ -13,6 +14,24 @@ hamburger.addEventListener('click', ()=>{
     hamburger.classList.toggle("toggle");
 });
 
+
+
+    /* Login */
+var x = document.querySelector(".usuario");
+var y = document.querySelector(".psswd");
+
+function sesion() {
+    if (x.value == "admin" && y.value == "admin") {
+        window.location.href = "administracion.html";
+        alert("Si");
+    }else{
+        window.location.href = "index.html";
+    }
+}
+
+
+
+    /* Categoria */
 $(function() {
 
     $('.toggles button').click(function(){
@@ -31,6 +50,8 @@ $(function() {
 
 }); 
 
+
+    /* Administracion */
 function edit_row(no)
 {
  document.getElementById("edit_button"+no).style.display="none";
@@ -90,71 +111,3 @@ function add_row()
  document.getElementById("new_stock").value="";
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //validacion contacto
-
-// const nombre = document.getElementById("name");
-// const email = document.getElementById("email");
-// const tef = document.getElementById("tef");
-// const mensaje = document.getElementById("mensaje");
-// const form = document.getElementById("formu");
-// const parraf = document.getElementById("warnings");
-
-// form.addEventListener("submit" , e=>{
-//     e.preventDefault();
-//     let warnings = "";
-//     let entrar = false;
-//     let regexNombre= /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
-//     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-//     let regexTef = /^\d{9}$/;
-//     let regexMensaje= /^[a-zA-ZÀ-ÿ\s]{1,120}$/;
-//     parraf.innerHTML = "";
-
-//     if(!regexNombre.test(nombre.value)){
-//         warnings += `El nombre no es válido <br>`;
-//         entrar = true;
-//     }
-
-//     if(!regexEmail.test(email.value)){
-//         warnings += `El email no es válido <br>`;
-//         entrar = true;
-//     }
-
-//     if(!regexTef.test(tef.value)){
-//         warnings += `El teléfono no es válido <br>`;
-//         entrar = true;
-//     }
-
-//     if(!regexMensaje.test(mensaje.value)){
-//         warnings += `Rellene el mensaje <br>`;
-//         entrar = true;
-//     }
-
-//     if (entrar){
-//         parraf.innerHTML = warnings;
-//     }else{
-//         parraf.innerHTML = "Enviado";
-//     }
-// });
