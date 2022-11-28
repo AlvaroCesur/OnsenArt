6a17deae92503321091912d46f702e8addac981a
Lizzy <alicia.s273596@cesurformacion.com>
2022-11-28 19:19:17 +0100

    administracion

--- js/main.js
+++ js/main.js
@@ -23,9 +23,10 @@ var y = document.querySelector(".psswd");
 function sesion() {
     if (x.value == "admin" && y.value == "admin") {
         window.location.href = "administracion.html";
-        alert("Si");
+        alert("Credenciales correctas");
     }else{
         window.location.href = "index.html";
+        alert("Credenciales correctas");
     }
 }
 
@@ -53,62 +54,77 @@ $(function() {
 
 
     /* Administracion */
-function edit_row(no)
-{
- document.getElementById("edit_button"+no).style.display="none";
- document.getElementById("save_button"+no).style.display="block";
-	
- var nombreProducto=document.getElementById("nombreProducto"+no);
- var precio=document.getElementById("precio"+no);
- var descripcion=document.getElementById("descripcion"+no);
- var stock=document.getElementById("stock"+no);
-	
- var nombreProducto_data=nombreProducto.innerHTML;
- var precio_data=precio.innerHTML;
- var descripcion_data=descripcion.innerHTML;
- var stock_data=stock.innerHTML;
-	
- nombreProducto.innerHTML="<input type='text' id='nombreProducto_text"+no+"' value='"+nombreProducto_data+"'>";
- precio.innerHTML="<input type='text' id='precio_text"+no+"' value='"+precio_data+"'>";
- descripcion.innerHTML="<input type='text' id='descripcion_text"+no+"' value='"+descripcion_data+"'>";
- stock.innerHTML="<input type='text' id='stock_text"+no+"' value='"+stock_data+"'>";
-}
-
-function save_row(no)
-{
- var nombreProducto_val=document.getElementById("nombreProducto_text"+no).value;
- var precio_val=document.getElementById("precio_text"+no).value;
- var descripcion_val=document.getElementById("descripcion_text"+no).value;
- var stock_val=document.getElementById("stock_text"+no).value;
-
- document.getElementById("nombreProducto"+no).innerHTML=nombreProducto_val;
- document.getElementById("precio"+no).innerHTML=precio_val;
- document.getElementById("descripcion"+no).innerHTML=descripcion_val;
- document.getElementById("stock"+no).innerHTML=stock_val;
-
- document.getElementById("edit_button"+no).style.display="block";
- document.getElementById("save_button"+no).style.display="none";
-}
-
-function delete_row(no)
-{
- document.getElementById("row"+no+"").outerHTML="";
-}
-
-function add_row()
-{
- var new_nombreProducto=document.getElementById("new_nombreProducto").value;
- var new_precio=document.getElementById("new_precio").value;
- var new_descripcion=document.getElementById("new_descripcion").value;
- var new_stock=document.getElementById("new_stock").value;
-	
- var table=document.getElementById("data_table");
- var table_len=(table.rows.length)-1;
- var row = table.insertRow(table_len).outerHTML="<tr id='row"+table_len+"'><td id='nombreProducto"+table_len+"'>"+new_nombreProducto+"</td><td id='precio"+table_len+"'>"+new_precio+"</td><td id='descripcion"+table_len+"'>"+new_descripcion+"</td><td id='stock"+table_len+"'>"+new_stock+"</td><td><input type='button' id='edit_button"+table_len+"' value='Edit' class='edit' onclick='edit_row("+table_len+")'> <input type='button' id='save_button"+table_len+"' value='Save' class='save' onclick='save_row("+table_len+")'> <input type='button' value='Delete' class='delete' onclick='delete_row("+table_len+")'></td></tr>";
-
- document.getElementById("new_nombreProducto").value="";
- document.getElementById("new_precio").value="";
- document.getElementById("new_descripcion").value="";
- document.getElementById("new_stock").value="";
-
-}
+    function edit_row(no)
+    {
+     document.getElementById("edit_button"+no).style.display="none";
+     document.getElementById("save_button"+no).style.display="inline";
+        
+     var id=document.getElementById("id"+no);
+     var nombre=document.getElementById("nombre"+no);
+     var precio=document.getElementById("precio"+no);
+     var descripcion=document.getElementById("descripcion"+no);
+     var stock=document.getElementById("stock"+no);
+        
+     var id_data=id.innerHTML;
+     var nombre_data=nombre.innerHTML;
+     var precio_data=precio.innerHTML;
+     var descripcion_data=descripcion.innerHTML;
+     var stock_data=stock.innerHTML;
+        
+     id.innerHTML="<input type='text' id='id_text"+no+"' value='"+id_data+"'>";
+     nombre.innerHTML="<input type='text' id='nombre_text"+no+"' value='"+ nombre_data+"'>";
+     precio.innerHTML="<input type='text' id='precio_text"+no+"' value='"+precio_data+"'>";
+     descripcion.innerHTML="<input type='text' id='descripcion_text"+no+"' value='"+descripcion_data+"'>";
+     stock.innerHTML="<input type='text' id='stock_text"+no+"' value='"+stock_data+"'>";
+    }
+    
+    function save_row(no)
+    {
+     var id_val=document.getElementById("id_text"+no).value;
+     var nombre_val=document.getElementById("nombre_text"+no).value;
+     var precio_val=document.getElementById("precio_text"+no).value;
+     var descripcion_val=document.getElementById("descripcion_text"+no).value;
+     var stock_val=document.getElementById("stock_text"+no).value;
+    
+     document.getElementById("id"+no).innerHTML=id_val;
+     document.getElementById("nombre"+no).innerHTML=nombre_val;
+     document.getElementById("precio"+no).innerHTML=precio_val;
+     document.getElementById("descripcion"+no).innerHTML=descripcion_val;
+     document.getElementById("stock"+no).innerHTML=stock_val;
+    
+     document.getElementById("edit_button"+no).style.display="inline";
+     document.getElementById("save_button"+no).style.display="none";
+    }
+    
+    function delete_row(no)
+    {
+     document.getElementById("row"+no+"").outerHTML="";
+    }
+    
+    function add_row()
+    {
+     var nuevo_id=document.getElementById("nuevo_id").value;
+     var nuevo_nombre=document.getElementById("nuevo_nombre").value;
+     var nuevo_precio=document.getElementById("nuevo_precio").value;
+     var nuevo_descripcion=document.getElementById("nuevo_descripcion").value;
+     var nuevo_stock=document.getElementById("nuevo_stock").value;
+        
+     var table=document.getElementById("data_table");
+     var table_len=(table.rows.length)-1;
+     var row = table.insertRow(table_len).outerHTML="<tr id='row"+table_len+"'><td id='id"+table_len+"'>"+nuevo_id+"</td><td id='nombre"+table_len+"'>"+nuevo_nombre+"</td><td id='precio"+table_len+"'>"+nuevo_precio+"</td><td id='descripcion"+table_len+"'>"+nuevo_descripcion+"</td><td id='stock"+table_len+"'>"+nuevo_stock+"</td><td><input type='button' id='edit_button"+table_len+"' value='Edit' class='edit' onclick='edit_row("+table_len+")'> <input type='button' id='save_button"+table_len+"' value='Save' class='save' onclick='save_row("+table_len+")' hidden> <input type='button' value='Delete' class='delete' onclick='delete_row("+table_len+")'></td></tr>";
+    
+     document.getElementById("nuevo_id").value="";
+     document.getElementById("nuevo_nombre").value="";
+     document.getElementById("nuevo_precio").value="";
+     document.getElementById("nuevo_descripcion").value="";
+     document.getElementById("nuevo_stock").value="";
+    }
+    
+    $("#editableTable").find("a[title='Delete']").click(function(e){  
+      var x;
+        if (confirm("Are you sure you want to delete entire row?") == true) {
+            $(this).closest("tr").remove();
+        } else {
+            
+        }     
+    });
